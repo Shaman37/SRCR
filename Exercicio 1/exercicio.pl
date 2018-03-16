69ab3d66b1293ac3da1d94294631b3bbd11f0e5e
Shaman37 <mrjohnnyloyal@gmail.com>
2018-03-16 16:48:32 +0000

    Query 1 2 e 3 funcionais

--- Exercicio 1/exercicio.pl
+++ Exercicio 1/exercicio.pl
@@ -1,10 +1,10 @@
-:- set_prolog_flag(discontiguous_warnings,off).
-:- set_prolog_flag(single_var_warnings,off).
-:- set_prolog_flag(unknown,fail).
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
 
-:- op(900,xfy,'::').
+:- op( 900,xfy,'::' ).
 :- dynamic utente/4.
-:- dynamic prestador/4.
+:- dynamic prestador/5.
 :- dynamic cuidado/5.
 
 %---------------------------------------------%
@@ -14,45 +14,38 @@
 % utente: #IdUt, Nome, Idade, Morada -> {V,F} % 
 utente(1,'Utente1',1,'primeiro').
 utente(2,'Utente2',2,'segundo').
-utente(3,'Utente3',3,'terceiro')
+utente(3,'Utente3',3,'terceiro').
 
 
 % prestador: #IdPrest, Nome, Especialidade, Instituição -> {V,F}
 
-prestador(1,'Prestador1','Especialidade1','Instituição1').
-prestador(2,'Prestador2','Especialidade2','Instituição2').
-prestador(3,'Prestador3','Especialidade3','Instituição3').
+prestador(1,'Prestador1','Especialidade1','Instituição1','cidade1').
+prestador(2,'Prestador2','Especialidade2','Instituição2','cidade2').
+prestador(3,'Prestador3','Especialidade3','Instituição3','cidade2').
 
 
 % cuidado: Data, #IdUt, #IdPrest, Descrição, Custo -> {V,F}
 
 cuidado('1-1-2018',1,1,'Era rabeta',2).
 cuidado('2-2-2018',2,2,'Tinha um lego no nariz',3).
-cuidado('2-2-2018',2,3,'Pisou o lego, depois de o ter tirado do nariz',4).
-
-
-%------------------------------------------------------------------%
-%-> Extensão do predicado que permite a procura de conhecimento  <-%
-%------------------------------------------------------------------%
-
-solutions(X,Y,Z) :- findall(X,Y,Z).
+cuidado('2-2-2018',2,2,'Pisou o lego, depois de o ter tirado do nariz',4).
 
 %------------------------------------------------------------------%
 %-> Extensão do predicado que permite a evolução do conhecimento <-%
 %------------------------------------------------------------------%
 
-insert(T) :- assert(T),
-             retract(T),!,fail.
+insert(T) :- assert(T).
+insert(T) :- retract(T),!,fail.
 
 test([]).
 test([H|T]) :- H,test(T).
 
-length([],0).
-length([H|T],R) :- length(T,C), R is 1+C.
+len([],0).
+len([H|T],R) :- len(T,C), R is 1+C.
 
-learn(Term) :- solutions(I,+F::I,L),
-	       insert(F),
-	       test(L).
+learn(T) :- solutions(I,+T::I,L),
+	    insert(T),
+	    test(L).
 
 %-----------------------------------------------------------------%
 %-> Extensão do predicado que permite a redução do conhecimento <-%
@@ -60,61 +53,122 @@ learn(Term) :- solutions(I,+F::I,L),
 
 remove(T) :- retract(T).
 
-forget(Term) :- solutions(I,+F::I,L),
-	        test(L),
-            	remove(F).
+forget(T) :- solutions(I,+T::I,L),
+	     test(L),
+             remove(T).
+
+%------------------------------------------------------------------%
+%-> Extensão do predicado que permite a procura de conhecimento  <-%
+%------------------------------------------------------------------%
+
+solutions(X,Y,Z) :- findall(X,Y,Z).
 
 %---------------------------------------------------------------------%
 %-> Invariante Estrutural do Utente (não permite inserção repetida) <-%
 %---------------------------------------------------------------------%
 
-+utente(ID,N,A,Z) :- (solutions(ID,utente(ID,_,_,_),L),
-		      length(L,N),
-		      N is 1).
++utente(ID,N,A,Z) :: (solutions(ID,(utente(ID,_,_,_)),L),
+		      len(L,C),
+		      C == 1).
 
 %------------------------------------------------------------------------%
 %-> Invariante Estrutural do Prestador (não permite inserção repetida) <-%
 %------------------------------------------------------------------------%
 
-+prestador(ID,N,S,I) :- (solutions(ID,prestador(ID,_,_,_),L),
-			 length(L,N),
-			 N is 1).
++prestador(ID,N,S,I) :: (solutions(ID,(prestador(ID,_,_,_,_)),L),
+			 len(L,C),
+			 C == 1).
 
 %----------------------------------------------------------------------%
 %-> Invariante Estrutural do Cuidado (não permite inserção repetida) <-%
 %----------------------------------------------------------------------%
 
-+cuidado(D,uID,pID,DSC,P) :- (solutions((D,uID,pID,DSC,P),cuidado(D,uID,pID,DSC,P),L),
-			      length(L,N),
-			      N is 1).
-		      
-		      
++cuidado(D,UID,PID,DG,P) :: (solutions((D,UID,PID,DG,P),(cuidado(D,UID,PID,DG,P)),L),
+			     len(L,C),
+			     C == 1).
+
++cuidado(D,UID,PID,DG,P) :: ((utente(UID,_,_,_)),(prestador(PID,_,_,_,_))).
+
 % Query 1
 %- Registar utentes
-utentRegist(id,n,a,z) :- learn(utente(id,n,a,z)).
+utentRegist(ID,N,A,Z) :- learn(utente(ID,N,A,Z)).
 %- Registar prestadores
-prestRegist(id,n,s,i) :- learn(prestador(id,n,s,i)).
+prestRegist(ID,N,S,I,C) :- learn(prestador(ID,N,S,I,C)).
 %- Registar cuidados
-cuidaRegist(d,uid,pid,dsc,p) :- learn(cuidado(d,uid,pid,dsc,p)).
+cuidaRegist(D,UID,PID,DSC,P) :- learn(cuidado(D,UID,PID,DSC,P)).
 
 % Query 2
 %- Remover utente
-utentRemove(uid) :- forget(utente(uid,_,_,_)).
+utentRemove(ID) :- forget(utente(ID,_,_,_)).
 % ao remover utentes -> removemos os cuidados a ele prestados?
 
 %- Remover prestador
-prestRemove(pid) :- forget(pretador(pid,_,_,_)).
+prestRemove(ID) :- forget(prestador(ID,_,_,_,_)).
 % ao remover prestadores -> removemos os cuidados por ele prestados?
 
 %- Remover cuidado
-cuidaRemove(d,uid,pid,dsc,p) :- forget(cuidado(d,uid,pid,dsc,p)).
+cuidaRemove(D,UID,PID,DG,P) :- forget(cuidado(D,UID,PID,DG,P)).
 
 % Query 3
 %- Identificar utentes por critérios
-utentID(ID,R) :- (solutions((ID,X,Y,Z),utente(,_,_,_),L)).
-utentName(N,R) :- (solutions((X,N,Y,Z),utente(_,nm,_,_),L)).
-utentAge(A,R) :- (solutions((X,Y,A,Z),utente(_,_,ag,_),L)).
-utenteZone(Z,R) :- (solutions((X,Y,W,Z),utente(_,_,_,zn),L)). 
+utentID(ID,R) :- (solutions((ID,N,A,Z),utente(ID,N,A,Z),R)).
+utentName(N,R) :- (solutions((ID,N,A,Z),utente(ID,N,A,Z),R)).
+utentAge(A,R) :- (solutions((ID,N,A,Z),utente(ID,N,A,Z),R)).
+utentZone(Z,R) :- (solutions((ID,N,A,Z),utente(ID,N,A,Z),R)). 
 
 % Query 4		 
+
+% Query 5
+%- Identificar cuidados de saúde prestados por instituição/cidade/datas
+
+%- Por instituicao
+cuidaInst(I,R) :- solutions(ID,prestador(ID,_,_,I,_),P),
+		  cuidaPrestador(P,R).
+
+%- Por cidade
+cuidaCid(C,R) :- solutions(ID,prestador(ID,_,_,_,C),P),
+		 cuidaPrestador(P,R).
+
+%- Por data
+cuidaData(D,R) :- solutions((D,IU,IP,DG,C),cuidado(D,IU,IP,DG,C),R).		      
+
+% Query 6
+%- Identificar os utentes de um prestador/especialidade/instituição
+
+%- prestador
+utentesPrestador(P,R) :- solutions(ID,cuidado(_,ID,P,_,_,_),R).
+utentesPrestador([P],R) :- utentesPrestador(P,R).
+utentesPrestador([P|Pp],R) :- utentesPrestador(P,R),
+			      utentesPrestador(Pp,R).
+%- especialidade
+utentesEspec(E,R) :- solutions(ID,prestador(ID,_,E,_),P),
+		     utentesPrestador(P,R).
+
+%- instituicao
+utentesInst(I,R) :- solutions(ID,prestador(ID,_,_,I),P),
+		    utentesPrestador(P,R).
+
+% Query 7
+%- Identificar cuidados de saúde realizados por utente/instituição/prestador
+
+%- utente
+cuidaUtente(U,R) :- solutions((D,IU,IP,D,C),cuidado(_,U,_,_,_),R).
+
+%- instituicao
+instCuida(I,R) :- solutions(ID,prestador(ID,_,_,I,_),P),
+		  cuidaPrestador(P,R).
+
+%- prestador
+cuidaPrestador([IP],R) :- solutions((D,IU,IP,DG,C),cuidado(D,IU,IP,DG,C),R).
+cuidaPrestador([IP|Pp],R) :- cuidaPrestador([IP],R),
+                             cuidaPrestador(Pp,R).	
+
+% Query 8
+%- Determinar todas as instituições/prestadores a que um utente já recorreu
+
+%- instituicoes
+
+%- prestadores
+prestUtentes(U,R) :- solutions(IP,cuidado(_,U,IP,_,_),R).
+
 
